8b12d5aeb956f995e263833b49f09b64b1f28dd2
Steve-Bright <swemhtet.net@gmail.com>
2025-09-12 15:42:54 +0800

    Unit Test completed

---
+++ Shapetest.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+using ShapeDrawer;
+using SplashKitSDK;
+
+[TestFixture()]
+public class ShapeTest
+{
+    [Test()]
+    public void TestShapeAt()
+    {
+        Shape s = new Shape();
+
+        s.X = 25;
+        s.Y = 25;
+        s.Width = 50;
+        s.Height = 50;
+
+        Assert.IsTrue(s.IsAt(SplashKit.PointAt(50, 50)));
+        Assert.IsTrue(s.IsAt(SplashKit.PointAt(25, 25)));
+        Assert.IsFalse(s.IsAt(SplashKit.PointAt(10, 50)));
+        Assert.IsFalse(s.IsAt(SplashKit.PointAt(50, 10)));
+    }
+
+    [Test()]
+    public void TestShapeAtWhenMovied()
+    {
+        Shape s = new Shape();
+
+        s.X = 25;
+        s.Y = 25;
+        s.Width = 30;
+        s.Height = 30;
+
+        Assert.IsTrue(s.IsAt(SplashKit.PointAt(25, 25)));
+
+        s.X = 100;
+        s.Y = 100;
+        Assert.IsFalse(s.IsAt(SplashKit.PointAt(25, 25)));
+    }
+
+    [Test()]
+    public void TestShapeAtWhenResized()
+    {
+        Shape s = new Shape();
+
+        s.X = 25;
+        s.Y = 25;
+        s.Width = 30;
+        s.Height = 30;
+
+        Assert.IsTrue(s.IsAt(SplashKit.PointAt(55, 55)));
+
+        s.Width = 10;
+        s.Height = 10;
+
+        Assert.IsFalse(s.IsAt(SplashKit.PointAt(55, 55)));
+        
+    }
+}
--- obj/Debug/net8.0/ShapeDrawer.AssemblyInfo.cs
+++ obj/Debug/net8.0/ShapeDrawer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34215629666c7d91a829e97535310ac4c92961c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShapeDrawer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
